bdcf7f12ab135456fa5d71b656cb67426e18381f
MichaelJuannn <juan600d@gmail.com>
2024-02-13 17:36:30 +0700

    Customer can view product list by product category

--- internals/service/product/product.go
+++ internals/service/product/product.go
@@ -8,9 +8,18 @@ import (
 )
 
 func GetAll(c *fiber.Ctx) error {
+	category := c.Query("category")
 	db := *database.DB
 	var products []model.Product
-	if err := db.Find(&products).Error; err != nil {
+	// fmt.Printf(" \n %s \n", category)
+	if category == "" {
+		print("NO QUERY")
+		if err := db.Find(&products).Error; err != nil {
+			c.Status(400).SendString("data not found")
+		}
+		return c.Status(200).JSON(products)
+	}
+	if err := db.Joins("INNER JOIN categories on products.category_id = categories.id").Where("categories.name = ?", category).Find(&products).Error; err != nil {
 		c.Status(400).SendString("data not found")
 	}
 	return c.Status(200).JSON(products)
